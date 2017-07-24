935f9b9875854d39b079e6ceeafc41c66907ff1c
Steffen Sauler <steffen@sauler.net>
2017-07-24 14:19:14 +0200

    Create send_mail.py

---
+++ katas/send_mail.py
@@ -0,0 +1,25 @@
+# Mail functionality
+SENDER = "steffen.sauler@"
+FROM = ""
+MAILSERVER = "mail.server.com"
+MSG_PRIORITY = '3'
+
+def send_mail(recipients, inp_msg, inp_subject):
+    """ Sends HTML mails """
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = inp_subject
+    msg['From'] = FROM
+    # Recipients are a list
+    msg['To'] = ", ".join(recipients)
+    msg["X-Priority"] = MSG_PRIORITY
+
+    part1 = MIMEText("Message is HTML", "plaintext")
+    part2 = MIMEText(inp_msg, 'html')
+
+    msg.attach(part1)
+    msg.attach(part2)
+
+    smtp_message = smtplib.SMTP(MAILSERVER)
+
+    smtp_message.sendmail(SENDER, recipients, msg.as_string())
